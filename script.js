424ffad642a99130b8ed6041706d85cc6a302757
NotFelBro <tagm554@gmail.com>
2026-08-11 20:58:25 -0300

    estruturando as funcoes de agendamento

--- script.js
+++ script.js
@@ -6,6 +6,20 @@ const imagensCarrosel = [
     "./imgs/Rectangle 13.png"
 ];
 
+const imagensColuna = [
+    "./imgs/Rectangle 20.png",
+    "./imgs/Rectangle 21.png",
+    "./imgs/Rectangle 72.png",
+    "./imgs/Rectangle 73_2x.png",
+    "./imgs/Rectangle 74.png",
+    "./imgs/Rectangle 75.png",
+    "./imgs/Rectangle 76.png",
+    "./imgs/Rectangle 78.png",
+    "./imgs/Rectangle 79.png",
+    "./imgs/Rectangle 80.png",
+    "./imgs/Rectangle 81.png"
+]
+
 function iniciarCarrosel() {
     const track = document.getElementById('carrosel-track');
     if (!track || imagensCarrosel.length === 0) return;
@@ -29,7 +43,17 @@ function criarImagemCarrosel(src) {
     return img;
 }
 
+function iniciarColuna() {
+    const track = document.getElementById('coluna-track');
+    if (!track || imagensColuna.length === 0) return;
+
+    imagensColuna.forEach((src) => {
+        track.appendChild(criarImagemCarrosel(src));
+    })
+}
+
 
 document.addEventListener('DOMContentLoaded', () => {
     iniciarCarrosel();
+    iniciarColuna();
 });
